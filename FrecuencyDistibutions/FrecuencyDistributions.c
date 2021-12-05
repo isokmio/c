e1806ab9227b92215d7db227f2b2ad0d802c785e
Jovanny <isokmio@gmail.com>
2021-12-05 09:04:12 -0500

    Add function for calculte acumulative frequencies

--- FrecuencyDistibutions/FrecuencyDistributions.c
+++ FrecuencyDistibutions/FrecuencyDistributions.c
@@ -6,7 +6,7 @@
 /**
  *  Return an array with the frecuency in each interval class
  */
-int* getFrecuencyArray(int *row, int rowSize, int qIntervals, int lowest, int intervalSize){
+int* getFrequencyArray(int *row, int rowSize, int qIntervals, int lowest, int intervalSize){
     int *frecuencies = malloc(qIntervals*sizeof(int));
     int j=0;
     
@@ -24,7 +24,7 @@ int* getFrecuencyArray(int *row, int rowSize, int qIntervals, int lowest, int in
 }
 
 /**
- * Display historgram in horizontal direction
+ * Display histogram in horizontal direction
  */
 void renderHistogram(int *frecuencyArray, int size, int intervalSize, int lowest, int high){
     int lowerLimit = lowest;
@@ -32,7 +32,7 @@ void renderHistogram(int *frecuencyArray, int size, int intervalSize, int lowest
     
     printf("\nHISTOGRAMA\n\n");
     
-    for(int i=0; i<size; i++){
+    for(int i=0; i<=size; i++){
         char line[high];
         for(int j=0; j<high; j++){
             line[j] = '\0';
@@ -47,6 +47,59 @@ void renderHistogram(int *frecuencyArray, int size, int intervalSize, int lowest
     }
 }
 
+/**
+ * Calculate the relative frequencies from row
+ **/
+double* relativeFrequencies(int *array, int size, int totalFrequency){
+    double *frequencies = malloc(size*sizeof(double));
+    for(int i=0; i<totalFrequency; i++){
+        frequencies[i] = (double)array[i]/(double)totalFrequency;
+    }
+    
+    return frequencies;
+}
+
+void renderRelatives(double *frequencies, int size, int intervalSize, int lowest){
+    int lowerLimit = lowest;
+    int upperLimit = lowest + intervalSize;
+    
+    printf("\n\nRELATIVE FREQUENCIES\n\n");
+    double error = 0.0;
+    for(int i=0; i<=size; i++){
+        printf("%3i-%3i: %4.2f%\n", lowerLimit, upperLimit, frequencies[i]*100);
+        error += frequencies[i];
+        lowerLimit = upperLimit;
+        upperLimit += intervalSize;
+    }
+    printf("\nError: %5.2f\n", (1- error)*100);
+}
+
+/**
+ * Acumulate the values for the frequencies calculated 
+ **/
+double* acumFrequencies(double *array, int size){
+    for (int i = 1; i < size; i++) {
+        array[i] = array[i-1] + array[i];
+    }
+    return array;
+}
+
+void renderAcumulative(double *array, int size){
+    for (int i = 0; i < size; i++) {
+        printf("%4.2f\n", array[i]);
+    }
+}
+
+void tRenderAcumulative(double *array, int size){
+    double acum = array[0];
+    int i = 1;
+    printf("%4.2f\n", acum);
+    do{
+        acum += array[i++];
+        printf("%4.2f\n", acum);
+    } while(i <= size);
+}
+
 int main(){
     int notes[SIZE];
     randomInit(notes, SIZE, 240);
@@ -58,7 +111,7 @@ int main(){
     int range = oRange(notes, SIZE);
     int intervalSize = getIntervalWidth(notes, SIZE);
     int intervals = range/intervalSize;
-    int *frecuencies = getFrecuencyArray(notes, SIZE, intervals, lowest, intervalSize);
+    int *frecuencies = getFrequencyArray(notes, SIZE, intervals, lowest, intervalSize);
 
     printf("The highest value in the notes are: %i.\n", higest);
     printf("The lowest value in the notes are: %i.\n", lowest);
@@ -67,11 +120,17 @@ int main(){
     printf("The number of intervals are: %i.\n\n", intervals);
     
     printArray(frecuencies, intervals);
-    
     for(int i=0; i<=intervals; i++){
         printf("%i ", lowest + i*intervalSize);
     }
-    
     renderHistogram(frecuencies, intervals, intervalSize, lowest, 50);
+    
+    double *rFrecuencies = relativeFrequencies(frecuencies, intervals, SIZE);
+    renderRelatives(rFrecuencies, intervals, intervalSize, lowest);
+
+    double *rAcumulative = acumFrequencies(rFrecuencies, intervals);
+    renderAcumulative(rAcumulative, intervals);
+
+    //tRenderAcumulative(rFrecuencies, intervals);
     return 0;
 }
