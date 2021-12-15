b37abfb323ba4e81c7675bd1c4c087240b0d472a
Jovanny <isokmio@gmail.com>
2021-12-14 21:56:00 -0500

    Add new functions to calculate geometric, harmonic and square mean

--- CentralTendency/CentralTendency.c
+++ CentralTendency/CentralTendency.c
@@ -8,11 +8,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct{
-  int frecuency;
-  double number;
-} Clazz;
+#include <math.h>
 
 double mean(double *data, int size){
     double acumulate = 0.0;
@@ -24,7 +20,6 @@ double mean(double *data, int size){
     return acumulate/size;
 }
 
-
 double weighted(double *data, double *weigth, int size){
     double acumulate = 0.0;
     double wsum = 0.0;
@@ -35,17 +30,6 @@ double weighted(double *data, double *weigth, int size){
         wsum += weigth[i];
     }
     return acumulate/wsum;
-}
-
-int fmean(Clazz *clazzes, int size){
-    double acumulate = 0.0;
-    double total = 0.0;
-    
-    for (int i = 0; i < size; i++) {
-        acumulate += clazzes[i].frecuency*clazzes[i].number;
-        total += clazzes[i].frecuency;
-    }
-    return acumulate/total;
 }
 
 double median(double *data, int size){
@@ -84,13 +68,45 @@ double mode(double *data, int size){
     return data[pivot];
 }
 
+double geometric(double *data, int size){
+    if(size==0) return 0.0;
+    double acumulate = 1.0;
+    double pivot = 1.0/size;
+    for (int i = 0; i < size; i++) {
+        acumulate *= data[i];
+    }
+    return pow(acumulate, pivot);
+}
+
+double harmonic(double *data, int size){
+    if(size==0) return 0.0;
+    double acumulate;
+    for (int i = 0; i < size; i++) {
+        acumulate += 1.0/data[i];
+    }
+    
+    return size/acumulate;
+}
+
+double square(double *data, int size){
+    if(size==0) return 0.0;
+    double acumulate;
+    for (int i = 0; i < size; i++) {
+        acumulate += pow(data[i], 2);
+    }
+    return sqrt(acumulate/size);
+}
+
+#define SIZE 4
 
 int main(){
-    double data[11] = {1.0, 2.0, 4.0, 3.0, 4.0, 4.0, 5.0, 1.0, 5.0, 5.0, 5.0};
+    double data[8] = {2.0, 4.0, 8.0, 2.0, 3.4, 1.0, 4.0, 2.5};
     double weigths[5] = {0.1, 0.3, 1.2, 3.2, 0.9};
     
-    printf("%3.2f", mode(data, 11));
     
+    printf("%3.2f - %3.2f = 3 (%3.2f - %3.2f)\n", mean(data, SIZE), mode(data, SIZE), mean(data, SIZE), median(data, SIZE));
+    printf("%3.2f <= %3.2f <= %3.2f\n", harmonic(data, 3), geometric(data, 3), mean(data, 3));
+    printf("%3.2f", square(data, 3));
     
     return 0;
 }
